770a0c9383fabbdea9b5076083027e290fe78e52
Guy Grinberger <ggrinber@akamai.com>
2019-06-11 10:30:43 +0300

    add properties to json extractor

--- ImageGrading/jsonPropetiesExtractor.py
+++ ImageGrading/jsonPropetiesExtractor.py
@@ -4,19 +4,22 @@ from os.path import isfile, join
 
 # face class to hold needed properties for each face
 class Face():
-   def __init__(self, joyLikelihood, rollAngle):
+   def __init__(self, detectionConfidence, blurredLikelihood, headwearLikelihood, joyLikelihood, rollAngle):
+      self.detectionConfidence = detectionConfidence
+      self.blurredLikelihood = blurredLikelihood
+      self.headwearLikelihood = headwearLikelihood
       self.joyLikelihood = joyLikelihood
       self.rollAngle = rollAngle
 
 # path for faces json files
-facesPath = '../FacesGroupingFromImage/output'
+facesPath = '/Users/ggrinber/BestCombinedImage/FacesGroupingFromImage/output'
 
 photoFiles = [f for f in listdir(facesPath) if isfile(join(facesPath, f))]
 photoFiles = [i[:1] for i in photoFiles]
 photoFiles.remove('d')
 
 # load json file
-with open('data.json', 'r') as f:
+with open(facesPath + '/data.json', 'r') as f:
     distros_dict = json.load(f)
 
 # extract properties out of json
@@ -25,6 +28,14 @@ for i in range(0, int(max(photoFiles))):
     print("Face number ", i)
 # with open('data.json', 'r') as f:
 #     distros_dict = json.load(f)
-    facesList.append(Face(distros_dict['images'][i]['faces'][i]['joyLikelihood'], distros_dict['images'][i]['faces'][i]['rollAngle']))
-    print("rollAngle: ", facesList[i].rollAngle)
+    facesList.append(Face(distros_dict['images'][i]['faces'][i]['detectionConfidence'], 
+                          distros_dict['images'][i]['faces'][i]['blurredLikelihood'], 
+                          distros_dict['images'][i]['faces'][i]['headwearLikelihood'],
+                          distros_dict['images'][i]['faces'][i]['joyLikelihood'],
+                          distros_dict['images'][i]['faces'][i]['rollAngle']))
+
+    print("detectionConfidence: ", facesList[i].detectionConfidence)
+    print("blurredLikelihood: ", facesList[i].blurredLikelihood)
+    print("headwearLikelihood: ", facesList[i].headwearLikelihood)
     print("joyLikelihood: ", facesList[i].joyLikelihood)
+    print("rollAngle: ", facesList[i].rollAngle)
