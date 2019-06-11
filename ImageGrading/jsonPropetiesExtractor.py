41140f511e2081e1001bfd356ede6db6b14d1f96
Guy Grinberger <ggrinber@akamai.com>
2019-06-11 08:39:12 +0300

    .

--- ImageGrading/jsonPropetiesExtractor.py
+++ ImageGrading/jsonPropetiesExtractor.py
@@ -2,20 +2,24 @@ import json                      # for json handling
 from os import listdir           # for files and system handling
 from os.path import isfile, join 
 
+# face class to hold needed properties for each face
 class Face():
    def __init__(self, joyLikelihood, rollAngle):
       self.joyLikelihood = joyLikelihood
       self.rollAngle = rollAngle
 
+# path for faces json files
 facesPath = '../FacesGroupingFromImage/output'
 
 photoFiles = [f for f in listdir(facesPath) if isfile(join(facesPath, f))]
 photoFiles = [i[:1] for i in photoFiles]
 photoFiles.remove('d')
 
+# load json file
 with open('data.json', 'r') as f:
     distros_dict = json.load(f)
 
+# extract properties out of json
 for i in range(0, int(max(photoFiles))):
     facesList = []
     print("Face number ", i)
