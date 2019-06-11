b251840d5f713a261fd581bd1d3f7fa66b45708d
Guy Grinberger <ggrinber@akamai.com>
2019-06-11 19:14:44 +0300

    added 'ImageGrading' path to config.py

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 BASE_DIR = os.path.normpath(os.path.dirname(__file__))
 FGFI_DIR = os.path.join(BASE_DIR, "FacesGroupingFromImage")
 IHS_DIR  = os.path.join(BASE_DIR, "ImageHeadsSwap")
+IMGG_DIR = os.path.join(BASE_DIR, "ImageGrading")
