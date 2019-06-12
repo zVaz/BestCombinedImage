e725426a831b7ee0777b695c6c6344e36048d9fc
Guy Grinberger <ggrinber@akamai.com>
2019-06-12 17:59:53 +0300

    use config.py file paths

--- ImageGrading/jsonExtractor.py
+++ ImageGrading/jsonExtractor.py
@@ -1,8 +1,11 @@
 import json                      # for json handling
 from os import listdir           # for files and system handling
 from os.path import isfile, join 
+import sys                      # for 'config.py' path
+sys.path.insert(0, '/Users/ggrinber/BestCombinedImage/')
+from config import BASE_DIR, FGFI_DIR, IMGG_DIR # for paths
 
-# face class to hold needed properties for each face
+# Attributes template
 class attributesT():
    def __init__(self, detectionConfidence, blurredLikelihood, headwearLikelihood, joyLikelihood, rollAngle):
       self.detectionConfidence = detectionConfidence
@@ -19,13 +22,13 @@ facesPath = homePath + 'FacesGroupingFromImage/output'
 propGradesPath = homePath + 'ImageGrading/propGrades.json'
 
 # get all output files
-photoFiles = [i[:1] for i in [f for f in listdir(facesPath) if isfile(join(facesPath, f))]]
+photoFiles = [i[:1] for i in [f for f in listdir(FGFI_DIR + '/output/') if isfile(join(FGFI_DIR + '/output/', f))]]
 photoFiles.remove('d')
 
 # extract updated grades for properties
 def getGradesWeight():
     # load properties grades weight json
-    with open(propGradesPath, 'r') as f:
+    with open(IMGG_DIR + '/propGrades.json', 'r') as f:
        grade_weight_json = json.load(f)
 
     return attributesT(grade_weight_json['properties'][0]['detectionConfidence'], 
@@ -37,7 +40,7 @@ def getGradesWeight():
 # extract properties out of json
 def extractFacesProperties():
     # load image data json
-    with open(facesPath + '/data.json', 'r') as f:
+    with open(FGFI_DIR + '/output/data.json', 'r') as f:
         image_data_json = json.load(f)
 
     for i in range(0, int(max(photoFiles))):
