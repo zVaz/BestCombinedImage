e29aa65ffeb293bff7bed1924113bbadb0773efe
Guy Grinberger <ggrinber@akamai.com>
2019-06-15 20:29:12 +0300

    IMGG - fix jsonExtractor and gradinSystem and added global main

---
+++ main.py
@@ -0,0 +1,8 @@
+from FacesGroupingFromImage.FacesGroupingFromImage import main as fgfi_main
+from ImageGrading.gradingSystem import main as imgg_main
+from ImageHeadsSwap.ImageHeadsSwap import main as ihs_main
+
+if __name__ == "__main__":
+    fgfi_main()
+    imgg_main()
+    ihs_main()
