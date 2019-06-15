4921836724dc7c69b64e237719d5a65571ab47da
Guy Grinberger <ggrinber@akamai.com>
2019-06-16 00:19:51 +0300

    added sightengine api

--- ImageGrading/gradingSystem.py
+++ ImageGrading/gradingSystem.py
@@ -1,7 +1,8 @@
 from os.path import isfile, join, dirname
 import sys                      # for 'config.py' path
 sys.path.append(dirname(__file__))
-from jsonExtractor import extractFacesProperties, getUpdatedGradesWeight, IMGG_DIR
+from jsonExtractor import extractFacesProperties, getUpdatedGradesWeight, IMGG_DIR, FGFI_DIR, imageToClass
+from ImageGrading.imageQualityDetection import set_image_properties
 import json
 import utils
 import os
@@ -13,37 +14,41 @@ if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
 
 def main():
-    # likeliness to grade weight dict   
-    strToGrade = { 'VERY_UNLIKELY' : 1,
-                    'UNLIKELY': 2,
-                    'POSSIBLE' : 3,
-                    'LIKELY' : 4,
-                    'VERY_LIKELY' : 5,
-                    'UNKOWN' : 0 }
-
     # get updated faces properties and grade weight
     groups = extractFacesProperties()
-    grades_weight = getUpdatedGradesWeight()
-
-    # initialize faceGrades
-    #faceGrades = [[0]*len(faces) for i in range(len(faces[0]))]
-    # get attributes to iterate over
-    #attributes = [a for a in dir(grades_weight) if not a.startswith('__')]
+    face_attrs, img_attrs = getUpdatedGradesWeight()
 
     best_faces = []
 
     for group in groups:
         for face in group:
-            face.set_grade(grades_weight)
+            face.set_grade(face_attrs)
         best_faces.append(max(group, key = lambda face: face.grade).face_info)
 
     count_bet_faces_in_image = {}
+
     for best_face in best_faces:
         curr = count_bet_faces_in_image.get(best_face["image_index"], 0)
         count_bet_faces_in_image[best_face["image_index"]] = curr + 1
 
     print(count_bet_faces_in_image)
-    best_image_index = max(count_bet_faces_in_image.items(), key = operator.itemgetter(1))[0]
+    # get images with the highest number of good faces
+    best_image_index = [k for k,v in count_bet_faces_in_image.items() if v == max(count_bet_faces_in_image.values())]
+
+    # if there are more than 1 image with highest number of good faces
+    if len(best_image_index) > 1:
+        with open(FGFI_DIR + '/output/data.json', 'r') as f:
+            image_data_json = json.load(f)
+        best_images_props = []
+        best_images = []
+        for best_image in best_image_index:
+            best_images_props.append(set_image_properties(image_data_json["images"][best_image]["path"]))
+            best_images.append(imageToClass(best_images_props[best_image], best_image))
+        for image_prop in best_images:
+            image_prop.set_grade(img_attrs)
+        best_image_index = max(best_images, key=operator.attrgetter('grade')).index
+    else:
+        best_image_index = max(count_bet_faces_in_image.items(), key = operator.itemgetter(1))[0]
 
     faces_to_swap = []
     for best_face in best_faces:
